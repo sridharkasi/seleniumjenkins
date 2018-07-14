4c8c04365b94b4e4b53c5be6a1774788dc621822
sridharkasi <sridharan.kasi@gmail.com>
2018-07-14 23:17:46 +0530

    commit

---
+++ src/main/java/SeleniumJenkins/SeleniumJenkins/printsomething.java
@@ -0,0 +1,13 @@
+package SeleniumJenkins.SeleniumJenkins;
+
+import org.testng.annotations.Test;
+
+public class printsomething {
+	@Test
+	public void print(){
+		System.out.println("##########################################");
+		System.out.println("######--JENKINS INTEGRATION -- #####");
+		System.out.println("########################################");
+	}
+
+}
--- src/main/java/SeleniumJenkins/SeleniumJenkins/seleniumTest.java
+++ src/main/java/SeleniumJenkins/SeleniumJenkins/seleniumTest.java
@@ -1,20 +1,22 @@
 package SeleniumJenkins.SeleniumJenkins;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class seleniumTest {
 	
-	 private WebDriver driver;
+	private WebDriver driver;
 	@Test
 	 public void launchbrowser(){
 		
-		 WebDriverManager.chromedriver().setup();
-		 driver = new ChromeDriver();
+		 //WebDriverManager.chromedriver().setup();
+		 //driver = new ChromeDriver();
 		 
+		driver = new HtmlUnitDriver();
 		 driver.get("http://www.google.com");
 	}
 
